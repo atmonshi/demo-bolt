41297ccd025c48db2e4f8bae5be1d60a83e0d197
Ash Monsh <wh7r.com@gmail.com>
2025-03-27 23:04:43 +0300

    add theme and updates

---
+++ resources/css/filament/admin/tailwind.config.js
@@ -0,0 +1,18 @@
+import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
+
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
+        './vendor/lara-zeus/core/resources/views/**/*.blade.php',
+
+        './vendor/lara-zeus/bolt-pro/resources/views/**/*.blade.php',
+
+        './vendor/lara-zeus/bolt/resources/views/themes/**/*.blade.php',
+        './vendor/lara-zeus/bolt/resources/views/filament/**/*.blade.php',
+
+        './vendor/lara-zeus/accordion/resources/views/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+}
